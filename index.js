3dd11e2120a1c35977eda906cc18c45554fd5fd9
lazyBit99 <alex.marchi99@gmail.com>
2019-04-16 12:06:07 +0200

    modifiche varie

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 const http = require('http')
+const port = 8081
 
 const server = http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content.Type': 'text/html' })
-    res.write('<h1>Alessandro Marchi</h1>')
-    res.end()
+    console.log(req.url)
+    if (req.url === '/') { //home
+        res.writeHead(200, {
+            'Content.Type': 'text/html'
+        })
+        res.write('<h1>Marchi Alessandro </h1>')
+        res.end()
+    } else if (req.url === '/api') {
+        res.writeHead(200, {
+            'Content.Type': 'text/html'
+        })
+        res.write(JSON.stringify(
+            [
+                {
+                    "nome": "Mario",
+                    "cognome": "Rossi"
+                },
+                {
+                    "nome": "Ken",
+                    "cognome": "Norris"
+                }
+            ]
+        ))
+        res.end()
+    } else {
+        res.writeHead(200, {
+            'Content.Type': 'text/html'
+        })
+        res.write(`<h1>${req.url}</h1>`)
+        res.end()
+    }
 })
 
-server.listen(8081, '127.0.0.1')
+server.listen(port, '127.0.0.1')
 
-console.log('Servver runnuning at http://127.0.0.1:8081/')
+console.log(`Servver runnuning at http://127.0.0.1:${port}/`)
