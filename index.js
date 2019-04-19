59274c4ab933e64af3dd0f0f6b9af6c4d2275ad6
lazyBit99 <alex.marchi99@gmail.com>
2019-04-19 11:43:35 +0200

    bug fixed

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express')
-let port = process.argv[3] || 3000;
+// let port = process.PORT || 3000
+let port = process.argv[2] || 3000
 const app = express()
 
 if (process.argv[2] === 'v')
   console.log('MyApp verisione 0.0.1')
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.use(express.static(__dirname + '/public'))
  
 app.listen(port)
 
