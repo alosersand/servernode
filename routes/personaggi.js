64f1475b6143b5d5b3608f7a425601613946f6e2
lazyBit99 <alex.marchi99@gmail.com>
2019-05-03 11:59:55 +0200

    query w/ key

--- routes/personaggi.js
+++ routes/personaggi.js
@@ -2,7 +2,24 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.send('Ciao dai personaggi')
+    const query = req.query
+    let msg = 'Ciao'
+    console.log('RES', query)
+
+    if (query.key === '768654343457870695854') {
+        msg = `${msg} Ale`
+
+        if (query.sesso === 'm') {
+            msg = `${msg} bell'uomo`
+        }
+        if (query.colore) {
+            msg = `${msg} ${query.colore}`
+        }
+    } else {
+        msg = 'Non sei autorizzato'
+    }
+
+    res.send(msg)
 })
   
 // router.get('/personaggi', (req, res) => {
